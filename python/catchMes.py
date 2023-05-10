71ca13ef04a527ab0ce566e1ccf082c905e261bf
NMD555 <nguyenminhdangvl@gmail.com>
2023-05-10 15:46:19 +0700

    update cachDatapython

--- python/catchMes.py
+++ python/catchMes.py
@@ -1,44 +1,46 @@
-import time
-import datetime
-import paho.mqtt.client as mqtt
 from pymongo import MongoClient
-from mongostream import MongoStream
+from pymongo.errors import PyMongoError
+from threading import Thread
+import threading
+import time
+import paho.mqtt.client as mqtt
+import datetime
 
-# Cài đặt thông tin kết nối MQTT
+IOTIDs =[]
+#mqtt
 mqtt_host = "iot-solar.nichietsuvn.com"
 mqtt_port = 1884
 mqtt_username = "guest"
 mqtt_password = "123456a@"
 
-# Cài đặt thông tin kết nối MongoDB
-mongo_host = "localhost"
-mongo_port = 27017
-mongo_database = "VTC"
-mongo_collection = "deviceData"
-device_info_collection = "deviceInfo"
+# Thiết lập kết nối đến MongoDB
+mongo_client = MongoClient('mongodb://localhost:27017')
+db = mongo_client['VTC']
+deviceInfoCol = db['deviceInfo']
+deviceDataCol = db['deviceData']
 
-# MQTT topics
-mqtt_topics = []
+mqtt_client = mqtt.Client()
 
-# Callback khi MQTT client kết nối thành công
 def on_connect(client, userdata, flags, rc):
     print("Connected to MQTT broker")
-    subscribe_to_topics(client)
 
-# Callback khi nhận được message từ MQTT broker
+def subscribe_to_topics(client):
+    #unsub truoc
+    for ID in IOTIDs:
+        mqtt_client.unsubscribe("server/"+ID+"/data")
+    for ID in IOTIDs:
+        mqtt_client.subscribe("server/"+ID+"/data")
+
+
 def on_message(client, userdata, msg):
     try:
-        # Kết nối tới cơ sở dữ liệu MongoDB
-        mongo_client = MongoClient(mongo_host, mongo_port)
-        db = mongo_client[mongo_database]
-        collection = db[mongo_collection]
-        
         # Lưu dữ liệu vào MongoDB cùng với thông tin thời gian nhận
         data = {
+            "id": msg.topic.split("/")[1],
             "payload": msg.payload.decode(),
             "timestamp": datetime.datetime.now()
         }
-        collection.insert_one(data)
+        deviceDataCol.insert_one(data)
         
         # Đóng kết nối tới cơ sở dữ liệu MongoDB
         mongo_client.close()
@@ -47,33 +49,50 @@ def on_message(client, userdata, msg):
     except Exception as e:
         print("Error saving data to MongoDB:", str(e))
 
-# Callback khi có thay đổi trong collection deviceInfo
-def on_change(change):
-    print("Change detected in deviceInfo collection:", change)
-    
-    # Unsubscribe các topic cũ
-    mqtt_client.unsubscribe(mqtt_topics)
-    
-    # Lấy danh sách các topic mới từ collection deviceInfo
-    new_topics = change["fullDocument"]["topics"]
-    
-    # Subscribe lại các topic mới
-    subscribe_to_topics(mqtt_client, new_topics)
-    
-    # Cập nhật danh sách các topic
-    mqtt_topics.clear()
-    mqtt_topics.extend(new_topics)
 
-# Hàm để subscribe vào các topic
-def subscribe_to_topics(client, topics):
-    client.subscribe([(topic, 0) for topic in topics])
-    print("Subscribed to topics:", topics)
 
-# Tạo MQTT client và thiết lập callback functions
-mqtt_client = mqtt.Client()
-mqtt_client.on_connect = on_connect
-mqtt_client.on_message = on_message
 
-# Thiết lập thông tin đăng nhập MQTT
-mqtt_client.username_pw_set(mqtt_username, mqtt_password)
+#vao db lay ID thiet bi
+def getIOTIDs():
+    IOTIDs = []
+    document = deviceInfoCol.find()
+    for doc in document:
+        IOTIDs.append(doc['deviceID'])
+    return IOTIDs
+
+# Tạo một pipeline để xác định các thay đổi muốn theo dõi
+def listenDeviceInfoChange():
+    global IOTIDs
+    try:
+        # Bắt đầu stream watch
+        with deviceInfoCol.watch() as stream:
+            for change in stream:
+                IOTIDs = getIOTIDs()
+                IOTIDs = list(set(IOTIDs)) # loai bo cac phan tu trung nha
+                subscribe_to_topics(mqtt_client)
+
+    except PyMongoError as e:
+        print("Lỗi xảy ra khi theo dõi thay đổi:", e)
+
+def saveDataDB():
+    global IOTIDs
+    # Tạo MQTT client và thiết lập callback functions
+    mqtt_client.username_pw_set(mqtt_username, mqtt_password)
+    mqtt_client.on_message = on_message
+
+    # Kết nối và lắng nghe dữ liệu từ MQTT broker
+    mqtt_client.connect(mqtt_host, mqtt_port)
+    IOTIDs = getIOTIDs()
+    IOTIDs = list(set(IOTIDs)) # loai bo cac phan tu trung nha
+    subscribe_to_topics(mqtt_client)
+    mqtt_client.loop_forever()
+
+if __name__ == '__main__':
+    t1 = threading.Thread(target=listenDeviceInfoChange)
+    t2 = threading.Thread(target=saveDataDB)
+    
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
 
