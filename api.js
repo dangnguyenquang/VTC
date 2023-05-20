34eeea3e979a7a706ab4146f253bb1c8e535fb05
Nguyen Quang Dang <dangnguyenquangit@gmail.com>
2023-05-20 08:48:15 +0700

    update API (interval).2

--- api.js
+++ api.js
@@ -178,7 +178,6 @@ function fetchDataAndSendAPI(deviceId) {
   getIntervalWithDeviceId(deviceId)
     .then((interval) => {
       const adjustedInterval = interval * 1000 * 3;
-      console.log(adjustedInterval);
 
       const now = new Date();
       const formattedDate = `${now.getDate().toString().padStart(2, '0')}/${(now.getMonth() + 1).toString().padStart(2, '0')}/${now.getFullYear()} ${now.getHours().toString().padStart(2, '0')}:${now.getMinutes().toString().padStart(2, '0')}:${now.getSeconds().toString().padStart(2, '0')}`;
