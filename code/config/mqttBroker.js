ae05137216b285a04707242def38d5e0bf3530fe
dangnguyenquang <dangnguyenquangit@gmail.com>
2023-10-02 20:42:58 +0700

    update mqtt broker

--- code/config/mqttBroker.js
+++ code/config/mqttBroker.js
@@ -1,7 +1,7 @@
 const mqtt = require("mqtt");
 
-const mqtt_broker = "iot-solar.nichietsuvn.com";
-const mqtt_port = 1884;
+const mqtt_broker = "iot-vtc.nichietsuvn.com";
+const mqtt_port = 1887;
 const mqtt_username = "guest";
 const mqtt_password = "123456a@";
 
